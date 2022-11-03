bbfd563c812366f802ca7265a1137711e3c560e4
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-03 22:46:29 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+for (const item of galleryItems) {
+  const link = document.createElement("a");
+  link.setAttribute("href", item.original);
+  link.classList.add("gallery__item");
+  const image = document.createElement("img");
+  image.setAttribute("src", item.preview);
+  image.setAttribute("alt", item.description);
+  //image.setAttribute("data-source", item.original);
+  image.classList.add("gallery__image");
+  link.append(image);
+  gallery.append(link);
+}
+console.log(gallery);
 console.log(galleryItems);

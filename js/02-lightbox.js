55024d1cdc37c5ea99a23fadb520eae46a8c8ad6
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-11-04 23:13:19 +0100

    task-02 update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,19 +2,29 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 let gallery = document.querySelector(".gallery");
 for (const item of galleryItems) {
-  const link = document.createElement("a");
-  link.setAttribute("href", item.original);
-  link.classList.add("gallery__item");
-  const image = document.createElement("img");
-  image.setAttribute("src", item.preview);
-  image.setAttribute("alt", item.description);
-  //image.setAttribute("data-source", item.original);
-  image.classList.add("gallery__image");
-  link.append(image);
-  gallery.append(link);
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__item" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}"
+        />
+      </a>`
+  );
+}
+gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+function showPic(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  gallery.on("show.simplelightbox", function () {});
 }
 
-gallery.next(); // Next Image
-
+gallery.addEventListener("click", showPic);
 console.log(gallery);
 console.log(galleryItems);
